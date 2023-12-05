91630873633828503c27c0bb4e121ded0e1fffe0
ANKUSH SHEKHAWAT <114976201+AnkushShekhawat2000@users.noreply.github.com>
2023-12-05 15:35:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
+// function firstNonRepeatedChar(str) {
+//  // Write your code here
+// }
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
+
+
 function firstNonRepeatedChar(str) {
- // Write your code here
-}
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+  const charFrequency = {};
+
+  // Count the frequency of each character
+  for (const char of str) {
+    charFrequency[char] = (charFrequency[char] || 0) + 1;
+  }
+
+  // Find the first character with a frequency of 1
+  for (const char of str) {
+    if (charFrequency[char] === 1) {
+      return char;
+    }
+  }
+
+  // Return
